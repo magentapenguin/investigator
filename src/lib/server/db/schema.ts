15a4b5ae358429fed8a7549d5fef0f6f0fa5060c
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2026-09-03 07:29:28 -0700

    add stuff

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,8 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const investgation = sqliteTable('investgation', {
+export const chat = sqliteTable('chat', {
 	id: text('id')
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	run: text('run', { mode: 'json' }).notNull()
+	messages: text('messages', { mode: 'json' }).notNull()
 });
